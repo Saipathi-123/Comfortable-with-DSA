ac5da3dfdbe9fb7e5bd707c887bd49ff4f3441f5
Sai Pathi <saipathi7400@gmail.com>
2026-09-09 11:21:49 +0530

    Time: 7 ms (99.36%), Space: 13.5 MB (56.36%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -4,13 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        count=0
-        count1=0
+        ans = 0
+        count = 0
         for i in nums:
-            if i==1:
-                count+=1
+            if i:
+                count = count + 1
             else:
-                count=0
-            count1=max(count,count1)
-        return count1
-        
+                if count > ans:
+                    ans = count
+                count = 0
+        if count > ans:
+            ans = count
+        return ans
