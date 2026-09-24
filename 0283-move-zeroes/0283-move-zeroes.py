761fd5fa18c3c3fcb7be099590f78e30a3f65b1e
Sai Pathi <saipathi7400@gmail.com>
2026-09-24 14:46:08 +0530

    Time: 3 ms (90.08%), Space: 13.7 MB (20.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,9 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        left= 0
-        for right in range(len(nums)):
-            if nums[right]!=0:
-                nums[left],nums[right]=nums[right],nums[left]
-                left+=1
+        write_ptr = 0
+        
+        # Step 1: Overwrite forward with non-zero elements
+        for read_ptr in range(len(nums)):
+            if nums[read_ptr] != 0:
+                nums[write_ptr] = nums[read_ptr]
+                write_ptr += 1
                 
+        # Step 2: Fill the remaining indices with zero
+        while write_ptr < len(nums):
+            nums[write_ptr] = 0
+            write_ptr += 1
