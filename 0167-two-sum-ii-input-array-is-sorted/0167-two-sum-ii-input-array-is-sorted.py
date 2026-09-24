1ef9e5f5b3782e9df9b168ff8e4a4b2dfdddad2f
Sai Pathi <saipathi7400@gmail.com>
2026-09-24 13:19:44 +0530

    Time: 6 ms (61.55%), Space: 14.5 MB (8.2%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,19 +1,20 @@
-
-
 class Solution(object):
-
-
     def twoSum(self, numbers, target):
-        # Your optimized two-pointer code goes here
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         left = 0
         right = len(numbers) - 1
         
         while left < right:
             current_sum = numbers[left] + numbers[right]
+            
             if current_sum == target:
-                return [left + 1, right + 1]
+                # Add 1 to convert from 0-based to 1-based indexing
+                return [left + 1, right + 1] 
             elif current_sum > target:
                 right -= 1
             else:
                 left += 1
-        return [left + 1, right + 1]
