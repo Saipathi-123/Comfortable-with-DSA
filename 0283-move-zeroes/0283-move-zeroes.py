2881de0c98668845780551affaeca777da806bef
Sai Pathi <saipathi7400@gmail.com>
2026-09-24 14:41:18 +0530

    Time: 8 ms (40.99%), Space: 13.6 MB (50.48%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,10 +7,6 @@ class Solution(object):
         left= 0
         for right in range(len(nums)):
             if nums[right]!=0:
-                temp=nums[left]
-                nums[left]=nums[right]
-                nums[right]=temp
+                nums[left],nums[right]=nums[right],nums[left]
                 left+=1
-                right+=1
-        return nums
-        
+                
