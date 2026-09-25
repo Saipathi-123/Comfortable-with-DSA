27cc726dee110124e4404a50a7c75b546064635a
Sai Pathi <saipathi7400@gmail.com>
2026-09-25 13:18:07 +0530

    Time: 15 ms (71.97%), Space: 13.6 MB (75.81%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -4,29 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        res=[]
-        for i in range(len(s)):
-            if s[i].lower() in "aeiou":
-                res.append(s[i])
-
-        l,r=0,len(res)-1
-        while l<r:
-            res[l],res[r]=res[r],res[l]
-            l+=1
-            r-=1
-
-        vowel_index=0
-        final_str=[]
-        for i in range(len(s)):
-            if s[i].lower() in "aeiou":
-                # Put the reversed vowel back in place
-                final_str.append(res[vowel_index])
-                vowel_index += 1
-            else:
-                # Keep the consonant exactly as it was
-                final_str.append(s[i])
-
-        # Convert list of characters back into a string
-        output = "".join(final_str)
-        return output
+        # Convert string to list so we can modify it in place
+        chars = list(s)
         
+        l, r = 0, len(chars) - 1
+        vowels = set("aeiouAEIOU")  # O(1) lookup time
+        
+        while l < r:
+            # Move left pointer until it hits a vowel
+            while l < r and chars[l] not in vowels:
+                l += 1
+            # Move right pointer until it hits a vowel
+            while l < r and chars[r] not in vowels:
+                r -= 1
+                
+            # Swap them directly in place
+            chars[l], chars[r] = chars[r], chars[l]
+            
+            l += 1
+            r -= 1
+            
+        return "".join(chars)
