d7073160498b0eca6699edf0c0d537a3fd19bb9e
Sai Pathi <saipathi7400@gmail.com>
2026-09-25 11:10:00 +0530

    Time: 3 ms (56.4%), Space: 12.3 MB (93.09%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -5,17 +5,17 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        counts = {}
-        result = []
-        
-        # Step 1: Count frequencies of elements in nums1
-        for num in nums1:
-            counts[num] = counts.get(num, 0) + 1
-            
-        # Step 2: Iterate through nums2 and collect common elements
-        for num in nums2:
-            if num in counts and counts[num] > 0:
-                result.append(num)
-                counts[num] -= 1  # Reduce the available frequency
-                
-        return result
+        nums1.sort()
+        nums2.sort()
+        res=[]
+        a,b=0,0
+        while a<len(nums1) and b<len(nums2):
+            if nums1[a]==nums2[b]:
+                res.append(nums1[a])
+                a+=1
+                b+=1
+            elif nums1[a]<nums2[b]:
+                a+=1
+            else:
+                b+=1
+        return res
